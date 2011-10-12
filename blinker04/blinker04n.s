380c321f38ec09330a7b80df0ac5392ac9da3b00
David Welch <dwelch@dwelch.com>
2011-10-12 01:16:54 -0400

    adding blinker03 and blinker04 examples, these use interrupts instead of polling

--- blinker04/blinker04n.s
+++ blinker04/blinker04n.s
@@ -18,7 +18,9 @@ BCSCTL2 equ 0x58
 TACTL   equ 0x0160
 TAR     equ 0x0170
 TACCR0  equ 0x0172
+TAIV    equ 0x012E
 TACCTL0 equ 0x0162
+
 
 P1OUT   equ 0x0021
 P1DIR   equ 0x0022
@@ -41,6 +43,7 @@ reset:
     bic.b #0x41,&P1OUT
     bis.b #0x40,&P1OUT
 
+
     ; 1MHz is 1000000 clocks per second
     ; 1000000 = 0xF4240
     ; The timers are 16 bit
@@ -54,38 +57,37 @@ reset:
     ; then, theory, we can measure seconds.
 
     bis.b #0x06,&BCSCTL2
-    mov #0x02C4,&TACTL
+    mov #0x10,&TACCTL0
     mov #0x3D08,&TACCR0
     mov #0x02D0,&TACTL
-    ;mov #0x02D0,&TACTL ; use this instead to blink faster
 
-loop:
+    bis.w #0x0018,r2
+    ; cpu should actually be off and not enter this loop
+forever:
+    jmp forever
+
+
+intled:
     xor.b #0x41,&P1OUT
-loop0:
-    bit.w #0x0001,&TACCTL0
-    jz loop0
-    bic.w #0x0001,&TACCTL0
-
-    jmp loop
-
+    reti
 
 hang:
     jmp hang
 
     org 0xFFE0
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw hang
-    dw reset
+    dw hang  ; 0xFFE0
+    dw hang  ; 0xFFE2
+    dw hang  ; 0xFFE4
+    dw hang  ; 0xFFE6
+    dw hang  ; 0xFFE8
+    dw hang  ; 0xFFEA
+    dw hang  ; 0xFFEC
+    dw hang  ; 0xFFEE
+    dw hang  ; 0xFFF0
+    dw intled; 0xFFF2
+    dw hang  ; 0xFFF4
+    dw hang  ; 0xFFF6
+    dw hang  ; 0xFFF8
+    dw hang  ; 0xFFFA
+    dw hang  ; 0xFFFC
+    dw reset ; 0xFFFE
